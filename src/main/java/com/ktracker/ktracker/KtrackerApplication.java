069f0aa0bcd14a2c14e3a54ba93a4e0f84aef236
PotatoTax <conor@johngregg.org>
2020-02-16 12:56:58 -0600

    Initial commit

---
+++ src/main/java/com/ktracker/ktracker/KtrackerApplication.java
@@ -0,0 +1,13 @@
+package com.ktracker.ktracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KtrackerApplication.class, args);
+    }
+
+}
